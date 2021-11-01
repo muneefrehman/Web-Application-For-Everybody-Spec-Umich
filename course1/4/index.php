f381979ff0a27f8e969834752c261201edf17f57
Syed Muneef ur Rehman <89536524+muneefrehman@users.noreply.github.com>
2021-11-01 19:04:33 +0500

    Add files via upload

---
+++ course1/4/index.php
@@ -0,0 +1,25 @@
+<head>
+  <meta charset="utf-8">
+  <title>Muneef Rehman PHP</title>
+</head>
+
+<body>
+  <h1>Muneef Rehman PHP</h1>
+  <?php
+    echo 'The SHA256 hash of "Muneef Rehman" is ';
+    echo hash('sha256','Muneef Rehman');
+  ?>
+
+  <pre>
+ASCII ART:
+
+    *                 *
+    *  *           *  *
+    *     *     *     *
+    *        *        *
+    *                 *
+    *                 *
+    *                 *
+  </pre>
+
+</body>  
---
+++ course1/5/guess.php
@@ -0,0 +1,33 @@
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Syed Muneef ur Rehman</title>
+</head>
+
+<body>
+  <h1>Welcome to my guessing game</h1>
+  <p>
+  <?php
+    if ( ! isset($_GET['guess']) ) {
+      echo ("Missing guess parameter");
+    }
+    else if ( strlen($_GET['guess'] ) < 1  ) {
+      echo ("Your guess is too short");
+    }
+    else if ( ! is_numeric($_GET['guess'] ) ) {
+      echo("Your guess is not a number");
+    }
+    else if ($_GET['guess'] < 20) {
+      echo ("Your guess is too low");
+    }
+    else if ($_GET['guess'] > 20) {
+      echo ("Your guess is too high");
+    }
+    else if ($_GET['guess'] == 20) {
+    echo ("Congratulations - You are right");
+    }
+    ?>
+  </p>
+
+</body>
+</html>
